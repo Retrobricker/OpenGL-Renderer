4b42f8e7c8ac66fab5f842148636b4acc9a5692c
Shujath Muqeem <smblolwutendstop@gmail.com>
2026-06-28 01:37:20 -0500

    Window creation working

--- main.cpp
+++ main.cpp
@@ -2,6 +2,10 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+    glViewport(0, 0, width, height);
+    }
+
 int main() {
     //Initialize glfw
     //Hints provide glfw what version the user has, so for OpenGL 3.3 we set the major and minor
@@ -19,7 +23,7 @@ int main() {
         return -1;
     }
     //Allows CPU to manage graphics for this specific window
-    glfwMakeContextCurrent();
+    glfwMakeContextCurrent(window);
 
     //GLAD manages the function pointers for OpenGL. We must initialize GLAD before making any
     //OpenGL calls
@@ -34,6 +38,7 @@ int main() {
     glViewport(0, 0, 800, 600);
 
     
+    
 
     //Tell GLFW we want to call the framebuffer_size_callback funciton every time it is resized
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
@@ -41,12 +46,17 @@ int main() {
     //Ready your engines! While loop so it stays open
     /*
     The glfwWindowShouldClose function checks at the start of each loop iteration if GLFW has been instructed to close. 
-    If so, the function returns true and the render loop stops running, after which we can close the application.
+    
     The glfwPollEvents function checks if any events are triggered 
-    (like keyboard input or mouse movement events), updates the window state, and 
-    calls the corresponding functions (which we can register via callback methods). 
-    The glfwSwapBuffers will swap the color buffer (a large 2D buffer that contains color values for each pixel in GLFW's window) 
+    (like keyboard input or mouse movement events)
+
+    The glfwSwapBuffers will swap the color buffer 
+    (a large 2D buffer that contains color values for each pixel in GLFW's window) 
     that is used to render to during this render iteration and show it as output to the screen.
+
+    Basically, instead of drawing the screen (left to right bottom to top) in front of the user,
+    it draws it on a second buffer. When it is done, it swaps the front and back buffer, so you
+    are always shown a fully drawn image.
     */
     while (!glfwWindowShouldClose(window)) {
         glfwSwapBuffers(window);
@@ -59,7 +69,5 @@ int main() {
     return 0;
 }
 
-void framebuffer_size_callback(GLFWWindow *window, int width, int height) {
-    glViewport(0, 0, width, height);
-}
+
 
